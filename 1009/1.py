0e919a119eab0a7d899f22a977f3c6550c70a072
JuliaJX <83075080+JuliaJX@users.noreply.github.com>
2021-09-20 09:56:54 +0300

    'Done'

--- 1009/1.py
+++ 1009/1.py
@@ -1,14 +1,31 @@
-import string
-s=string.ascii_lowercase
-
-l=list(s)
-def functial1 (l): 
-    for el in l:
-        print(el)
-
-functial1 (l)
-
 
 import string
+s = string.ascii_lowercase
 
-s=string.ascii_lowercase
+l = [c for c in s]
+
+def f(l):
+    for d in l:
+        print(d)
+
+
+def f2(l):
+    i=0
+    while i<len(l):
+        print(l[i])
+        i+=1
+
+
+def dec(leng):
+    def new_leng(*args,**kwargs):
+        print('Running function: ', leng.__name__)
+        print('Positional arguments are: ', args)
+        print('Keyword arguments are: ', kwargs)
+        result = leng(*args, **kwargs)
+        print('Result: ', result)
+        return result
+    return new_leng
+
+f2_dec = dec(f2)
+
+f2_dec(l)
